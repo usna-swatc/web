d029853c90af5af67cf95015e56341db537f9d1a
MIDN Jake Woods <woods.jake2019@gmail.com>
2019-04-17 09:02:32 -0400

    goto

--- basic/goto.php
+++ basic/goto.php
@@ -9,13 +9,62 @@
 ?>
 
 <html>
+  <p id="testing"></p>
   <script type="text/javascript" src="http://static.robotwebtools.org/EventEmitter2/current/eventemitter2.min.js"></script>
   <script type="text/javascript" src="http://static.robotwebtools.org/roslibjs/current/roslib.min.js"></script>
   <script type="text/javascript" type="text/javascript">
+    function success(pos) {
+      userlat = pos.coords.latitude;
+      userlong = pos.coords.longitude;
+      jQuery('#userlat').val(userlat);
+      jQuery('#userlong').val(userlong);
+    }
+    // if error, defualt USNA
+    function error(err) {
+      userlat = 38.9836;
+      userlong = -76.4823;
+      jQuery('#userlat').val(userlat);
+      jQuery('#userlong').val(userlong);
+    }
 
-    // connect to ROS, needs to be changed to the O-Droid
     var ros = new ROSLIB.Ros({
       url : 'ws://localhost:9090'
+    });
+
+    var page = document.getElementById("testing")
+
+    var userlat;
+    var userlong;
+
+    //get location of the user
+    navigator.geolocation.getCurrentPosition(success, error);
+page.innerHTML += "here";
+    page.innerHTML += jQuery('#userlat').val();
+    page.innerHTML += "a\n";
+    page.innerHTML += jQuery('#userlong').val();
+    page.innerHTML += "a\n";
+page.innerHTML += "here";
+
+    var goToClient = new ROSLIB.ActionClient({
+      ros : ros,
+      serverName : '/goto',
+      actionName : 'goats-msgs/action/goto.action'
+    });
+
+    var goal = new ROSLIB.Goal({
+      actionClient : goto.action,
+      goalMessage : {
+        lattitude: 50,
+        longitude: 50
+      }
+    });
+
+    goal.on('feedback', function(feedback) {
+      console.log('Feedback: ' + feedback.sequence);
+    });
+
+    goal.on('result', function(result) {
+      console.log('Final Result: ' + result.sequence);
     });
 
     ros.on('connection', function() {
@@ -30,20 +79,6 @@
       console.log('Connection to websocket server closed.');
     });
 
-    var cmdTopic = new ROSLIB.Topic({
-      ros : ros,
-      name : '/cmd_topic',
-      messageType : '../goats-msgs/Task.msg'
-    })
-
-    var msg = new ROSLIB.Message({
-      target_group : 0,
-      target_sys : 0,
-      target_task : 'goto.action'
-    });
-    cmdTopic.publish(msg);
-
-    window.location.href = "../basic.php";
-
+    goal.send();
   </script>
 </html>
